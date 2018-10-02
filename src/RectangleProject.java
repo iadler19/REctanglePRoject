7f14f3d223e963dbaf56de73e89cf8b38308b732
iadler19 <42978499+iadler19@users.noreply.github.com>
2018-10-02 14:29:17 -0400

    Add files via upload

---
+++ src/RectangleProject.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RectangleProject {
+	
+	public static void main(String[] args)
+	
+	{
+		Scanner in = new Scanner(System.in);
+		
+		double length = in.nextInt();
+		in.nextLine();
+		double width = in.nextInt();
+		in.nextLine();
+		double area = length *  width;
+		System.out.printf
+	}
+
+}
