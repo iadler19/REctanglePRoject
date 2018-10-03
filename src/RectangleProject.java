3ee0d66e695e4273e61c505e5030319281dfe276
iadler19 <42978499+iadler19@users.noreply.github.com>
2018-10-03 14:11:19 -0400

    Add files via upload

--- src/RectangleProject.java
+++ src/RectangleProject.java
@@ -6,13 +6,18 @@ public class RectangleProject {
 	
 	{
 		Scanner in = new Scanner(System.in);
-		
+		System.out.println("Enter Length:");
 		double length = in.nextInt();
 		in.nextLine();
+		
+		System.out.println("Enter Width");
 		double width = in.nextInt();
 		in.nextLine();
 		double area = length *  width;
-		System.out.printf
+		double perimeter = 2 * length + 2 * width;
+		double diagonals = Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2));
+		System.out.printf("Area:%13.2f \nPerimeter:%8.2f  \nDiagonal:%9.2f", area, perimeter, diagonals);
 	}
 
 }
+
